37292ac4ed0073c1cb01171cbceb3b8b94178dd2
solhermelo <50157862+solhermelo@users.noreply.github.com>
2020-07-07 00:08:28 -0300

    Add files via upload

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,37 +1,29 @@
 $(document).ready(function(){
-    // $('.js--mobile-nav-icon').on('click', function(){
-    //     $('.main-nav').toggle("fast");
-    // });
+    $('.js--mobile-nav-icon').on('click', function(){
+        $('nav').toggle("");
+    });
 
     $('.js--mobile-nav-1').click(function(){
         $('.dropdown-1').toggle();
-
-        $(document).on("click", function(event){
-            var $trigger = $(".js--mobile-nav-1");
-            if($trigger !== event.target && !$trigger.has(event.target).length){
-                $(".dropdown-1").slideUp("");
-            }            
-        });
+    });
+    
+    $(document).on("click", function(event){
+        var $triggerDos = $(".js--mobile-nav-2");
+        if($triggerDos !== event.target && $triggerDos.has(event.target).length){
+            $(".dropdown-1").slideUp("");
+        }            
     });
 
     $('.js--mobile-nav-2').click(function(){
         $('.dropdown-2').toggle();
-
-        $(document).on("click", function(event){
-            var $trigger = $(".js--mobile-nav-2");
-            if($trigger !== event.target && !$trigger.has(event.target).length){
-                $(".dropdown-2").slideUp("");
-            }            
-        });
     });
 
-
-        $(document).on("click", function(event){
-            var $trigger = $(".drop-active");
-            if($trigger !== event.target && !$trigger.has(event.target).length){
-                $(".drop-active").removeClass("drop-active");
-            }            
-        });
+    $(document).on("click", function(event){
+        var $triggerDos = $(".js--mobile-nav-1");
+        if($triggerDos !== event.target && $triggerDos.has(event.target).length){
+            $(".dropdown-2").slideUp("");
+        }             
+    });
 
 });
 
